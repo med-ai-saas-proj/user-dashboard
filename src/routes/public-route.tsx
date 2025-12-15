91b11268f4be3e39e8de4212a4f447aa72883cd4
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-12-15 19:27:41 +0700

    rf: remove unneeded any

--- src/routes/public-route.tsx
+++ src/routes/public-route.tsx
@@ -21,7 +21,7 @@ export const PublicRoute = ({ children }: PublicRouteProps) => {
   }
 
   if (authenticated) {
-    const from = (location.state as any)?.from?.pathname || '/';
+    const from = location.state?.from?.pathname || '/';
     return <Navigate to={from} replace />;
   }
 

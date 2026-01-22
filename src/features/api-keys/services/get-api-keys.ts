40c52f6c1ba4529f7105b36819247d14495e0f48
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2026-01-22 10:20:42 +0700

    feat: integrate get api keys endpoint

--- src/features/api-keys/services/get-api-keys.ts
+++ src/features/api-keys/services/get-api-keys.ts
@@ -1,10 +1,18 @@
 import { API_ROUTES } from '@/config/api-routes';
 import type { GetApiKeyResponse } from '@/features/api-keys/services/api-key.dto';
+import type { APIKey } from '@/features/api-keys/api-key.type';
 import apiClient from '@/query/api-client';
 
-export const getApiKeys = async () => {
-  const { data } = await apiClient.get<GetApiKeyResponse>(
-    API_ROUTES.MANAGEMENT.API_KEYS
-  );
-  return data;
+export const getApiKeys = async (): Promise<APIKey[]> => {
+	const { data } = await apiClient.get<GetApiKeyResponse>(
+		API_ROUTES.MANAGEMENT.API_KEYS
+	);
+	return data.map((apiKey) => ({
+		id: apiKey.id,
+		name: apiKey.name,
+		description: apiKey.description,
+		hint: apiKey.hint,
+		createdAt: new Date(apiKey.created_at),
+		permissions: apiKey.permissions,
+	}));
 };

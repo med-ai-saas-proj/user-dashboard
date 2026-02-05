f5137878d676b4616dd550dbe211922fdda2ebcb
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2026-02-05 08:43:12 +0700

    rf: remove all old request code

--- src/features/pg-rx-advisor/hooks/use-rx-advisor.ts
+++
@@ -1,9 +0,0 @@
-import { useMutation } from '@tanstack/react-query';
-import { getRxAdvisor } from '@/features/pg-rx-advisor/services/get-rx-advisor';
-import type { RxAdvisorRequest } from '../services/rx-advisor.dto';
-
-export const useRxAdvisor = () => {
-  return useMutation({
-    mutationFn: (request: RxAdvisorRequest) => getRxAdvisor(request),
-  });
-};

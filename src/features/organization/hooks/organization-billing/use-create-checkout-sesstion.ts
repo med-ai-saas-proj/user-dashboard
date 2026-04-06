727e629ec3bc137911cc78d3c503e2ea51f4d5f2
PineGithub19 <114576524+PineGithub19@users.noreply.github.com>
2026-04-06 18:05:41 +0100

    chore: some update for routes and hooks in billing

--- src/features/organization/hooks/organization-billing/use-create-checkout-sesstion.ts
+++ src/features/organization/hooks/organization-billing/use-create-checkout-sesstion.ts
@@ -2,8 +2,8 @@ import { useMutation } from "@tanstack/react-query";
 import { createCheckoutSession } from "../../services/organization-billing/create-checkout-session";
 
 export const useCreateCheckoutSession = () => {
-	return useMutation<string>({
+	return useMutation({
 		mutationKey: ["createCheckoutSession"],
-		mutationFn: () => createCheckoutSession(),
+		mutationFn: createCheckoutSession,
 	});
 };

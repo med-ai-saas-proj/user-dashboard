6ee5353463494dcc346c67ce17c41c89182d4f38
Hung Nguyen <nthungdev@gmail.com>
2026-06-01 14:07:31 -0400

    style: section 1 2 3

--- src/routes/whitepaper.tsx
+++ src/routes/whitepaper.tsx
@@ -25,7 +25,7 @@ function Section({
 	return (
 		<section
 			className={cn(
-				"grid grid-cols-1 md:grid-cols-[74px_1fr] gap-5 space-y-10 py-12 md:py-16",
+				"grid grid-cols-1 md:grid-cols-[74px_1fr] gap-5 space-y-10 py-12 md:py-16 gap-x-20",
 				className
 			)}
 		>
@@ -36,7 +36,7 @@ function Section({
 			</div>
 			<div>
 				<div className="flex flex-row gap-4 md:gap-10 mb-6">
-					<span className="font-serif text-2xl leading-tight text-zinc-900 md:mt-5 md:text-4xl">
+					<span className="md:hidden font-serif text-2xl leading-tight text-zinc-900 md:mt-5 md:text-4xl">
 						{number}
 					</span>
 					<h2 className="font-serif text-2xl leading-tight text-zinc-900 md:mt-5 md:text-4xl">
@@ -136,32 +136,46 @@ export default function WhitepaperPage() {
 
 					<Section number={t("section1.number")} title={t("section1.title")}>
 						<div className="space-y-6">
-							<div className="grid md:grid-cols-4">
-								{section1.steps.map((item, index) => (
-									<div
-										key={`${section1.number}-challenge-${index}`}
-										className="border-t last:border-b border-zinc-300 py-2"
-									>
-										<p className="text-xs leading-relaxed text-zinc-700 md:text-sm">
-											{item}
-										</p>
-									</div>
-								))}
+							<div>
+								<div className="grid md:grid-cols-4">
+									{section1.steps.map((item, index) => (
+										<div
+											key={`${section1.number}-challenge-${index}`}
+											className="flex flex-row md:flex-col justify-start gap-y-2"
+										>
+											<div className="relative mr-3 w-4 md:w-full">
+												<div className="relative mx-auto md:mx-0 size-1.5 rounded-full bg-black" />
+												{/* dotted vertical line for mobile, horizontal line for desktop */}
+												<div className="md:hidden absolute left-1/2 top-0 h-full -translate-x-1/2 border-l border-dotted border-black" />
+												<div className="hidden md:block absolute top-1/2 -translate-y-1/2 w-full border-t border-dotted border-black" />
+											</div>
+											<p className="flex-1 text-xs leading-relaxed text-zinc-700 md:text-sm -mt-1 pb-2 md:pr-4">
+												{item}
+											</p>
+										</div>
+									))}
+								</div>
 							</div>
 
 							<div className="flex flex-row justify-start items-start gap-2 md:gap-6 ">
 								<div>
-									<ArrowRightIcon className="size-10 text-zinc-500" />
+									<ArrowRightIcon className="size-6 md:size-10 text-zinc-500" />
 								</div>
-								<p className="max-w-[760px] font-serif text-2xl leading-tight text-zinc-800 md:text-3xl">
+								<p className="max-w-[760px] font-serif text-xl leading-tight text-zinc-800 md:text-3xl">
 									{section1.body1}
 								</p>
 							</div>
 
+							<hr className="border-t border-zinc-200" />
+
 							<div>
-								<p className="mt-4">{section1.remark}</p>
-								<p>{section1.body2}</p>
-								<div>
+								<div className="space-y-2">
+									<p className="mt-4 text-[#6E6E6E] italic">
+										{section1.remark}
+									</p>
+									<p className="text-xl md:text-4xl">{section1.body2}</p>
+								</div>
+								<div className="mt-4 space-y-4">
 									<p>{section1.body3}</p>
 									<p>{section1.body4}</p>
 								</div>
@@ -171,11 +185,10 @@ export default function WhitepaperPage() {
 
 					<Section number={section2.number} title={section2.title}>
 						<div>
-							<div>
+							<div className="space-y-2">
 								<p className="md:text-[19px] font-semibold leading-relaxed text-zinc-700">
 									{section2.description}
 								</p>
-
 								<p className="text-[#1f1f1f] md:text-[17px]">
 									{section2.example}
 								</p>
@@ -206,28 +219,45 @@ export default function WhitepaperPage() {
 							</div>
 
 							<div className="mt-12 md:mt-24">
-								<h3 className="uppercase">{section2.coverage.title}</h3>
-								<div>
-									<p>{section2.coverage.subtitle}</p>
+								<h3 className="uppercase text-2xl md:text-4xl font-semibold">
+									{section2.coverage.title}
+								</h3>
+								<div className="mt-4 md:mt-2 flex flex-col md:flex-row md:space-between gap-y-2 gap-x-4">
+									<p className="text-xl md:text-3xl">
+										{section2.coverage.subtitle}
+									</p>
 									<p>{section2.coverage.description}</p>
 								</div>
 							</div>
 
-							<div>
-								<h3>{section2.surfaces.title}</h3>
-								<p>{section2.surfaces.subtitle}</p>
-								<div>
-									{section2.surfaces.products.map(
-										({ title, description }, index) => (
-											<div
-												key={`section2-surface-product-${index}`}
-												className="mt-6"
-											>
-												<h4>{title}</h4>
-												<p>{description}</p>
+							<div className="mt-8">
+								<h3 className="uppercase text-2xl md:text-4xl font-semibold">
+									{section2.surfaces.title}
+								</h3>
+								<p className="mt-4 md:mt-2 text-xl md:text-3xl">
+									{section2.surfaces.subtitle}
+								</p>
+
+								<div className="mt-8 grid lg:grid-cols-5">
+									{section2.surfaces.products.map((item, index) => (
+										<div
+											key={`${section1.number}-challenge-${index}`}
+											className="flex flex-row lg:flex-col justify-start gap-y-2"
+										>
+											<div className="relative mr-3 w-4 lg:w-full">
+												<div className="relative mx-auto lg:mx-0 size-1.5 rounded-full bg-black" />
+												{/* dotted vertical line for mobile, horizontal line for desktop */}
+												<div className="lg:hidden absolute left-1/2 top-0 h-full -translate-x-1/2 border-l border-dotted border-black" />
+												<div className="hidden lg:block absolute top-1/2 -translate-y-1/2 w-full border-t border-dotted border-black" />
 											</div>
-										)
-									)}
+											<div className="flex flex-col gap-y-2 pb-1">
+												<div>{item.title}</div>
+												<p className="flex-1 text-xs leading-relaxed text-zinc-700 md:text-sm -mt-1 pb-2 md:pr-4">
+													{item.description}
+												</p>
+											</div>
+										</div>
+									))}
 								</div>
 							</div>
 						</div>
@@ -235,57 +265,89 @@ export default function WhitepaperPage() {
 
 					<Section number={section3.number} title={section3.title}>
 						<p>{section3.body1}</p>
+						<p className="mt-4">{section3.body2}</p>
 
-						<p>{section3.body2}</p>
-
-						<div>
+						<div className="mt-4">
 							{section3.text.map((item, index) => (
-								<p
-									key={`${section3.number}-text-${index}`}
-									className="mt-4 text-sm leading-relaxed text-zinc-700"
-								>
-									{item}
-								</p>
-							))}
-						</div>
-
-						<div className="space-y-4">
-							{section3.steps.map((step) => (
 								<div
-									key={`${step.title}-tile`}
-									className="rounded-xl border border-zinc-300 bg-zinc-50 p-5"
+									className="border-t border-zinc-200 py-4 flex flex-row"
+									key={`${section3.number}-text-${index}`}
 								>
-									<p className="mt-2 text-sm font-semibold">{step.title}</p>
+									<div className="w-10 md:w-14 text-sm italic text-[#6E6E6E] ">
+										{(index + 1).toString().padStart(2, "0")}
+									</div>
+									<p className="flex-1 text-sm leading-relaxed font-semibold">
+										{item}
+									</p>
 								</div>
 							))}
 						</div>
 
-						<div>
-							<p>{section3.figures.title}</p>
-							<div>
-								{section3.figures.steps.map((item) => (
-									<div
-										key={`${section3.number}-figure-${item.title}`}
-										className="mt-4"
-									>
-										<p className="text-sm font-medium text-zinc-700">
+						<div className="mt-8">
+							{section3.steps.map((item, index) => (
+								<div
+									key={`${section1.number}-challenge-${index}`}
+									className="flex justify-start gap-y-2"
+								>
+									<div className="relative mr-3 w-4">
+										{/* dotted vertical line for mobile, horizontal line for desktop */}
+										<div className="absolute left-1/2 top-0 h-full -translate-x-1/2 border-l border-dotted border-black" />
+										<div className="relative mx-auto size-3 rounded-full border border-black bg-[#F3F3F3]" />
+									</div>
+									<div className="flex flex-col pb-8 gap-y-2.5">
+										<div className="italic text-[#0A0A0A] -mt-1 text-sm">
 											{item.title}
-										</p>
-										<p className="text-sm leading-relaxed text-zinc-700">
+										</div>
+										<p className="md:text-lg font-semibold">{item.subtitle}</p>
+										<p className="md:text-lg leading-relaxed text-zinc-700 pb-2 md:pr-4">
 											{item.body}
 										</p>
 									</div>
+								</div>
+							))}
+						</div>
+
+						<div className="mt-10">
+							<p className="text-[#6E6E6E] italic">{section3.figures.title}</p>
+							<div className="mt-6 space-y-4 grid grid-cols-1 md:grid-cols-[1fr_48px_1fr_48px_1fr] gap-x-0">
+								{section3.figures.steps.map((item, index) => (
+									<>
+										<div
+											key={`${section3.number}-figure-${item.title}`}
+											className="h-full bg-[#F3F3F3] px-4 py-4 text-center flex flex-col items-center"
+										>
+											<div className="size-10 text-white rounded-full bg-black shrink-0 grow-0 flex items-center justify-center text-lg">
+												{item.title.charAt(0)}
+											</div>
+											<p className="mt-4 text-lg font-medium text-zinc-700">
+												{item.title}
+											</p>
+											<p className="mt-2 text-sm leading-relaxed text-zinc-700">
+												{item.body}
+											</p>
+										</div>
+										{index !== section3.figures.steps.length - 1 && (
+											<div
+												key={`arrow-${index}`}
+												className="flex flex-col justify-center items-center"
+											>
+												<ArrowRightIcon className="size-6" />
+											</div>
+										)}
+									</>
 								))}
 							</div>
 						</div>
 
-						<div>
+						<hr className="border-dotted border-t border-t-[#A3A3A3] mt-10" />
+
+						<div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-4">
 							{section3.points.map((item, index) => (
 								<p
 									key={`${section3.number}-point-${index}`}
-									className="mt-4 text-sm leading-relaxed text-zinc-700"
+									className="text-sm leading-relaxed text-zinc-700 border-l-2 border-l-red-800 p-4 bg-[#F3F3F3]"
 								>
-									{item}
+									<span className="font-semibold">{item}</span>
 								</p>
 							))}
 						</div>

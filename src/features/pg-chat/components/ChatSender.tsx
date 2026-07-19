9f024bccefceaac3f0449dafaf12c2072d3f2290
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2026-07-19 21:21:04 +0700

    chore: remove pg code

--- src/features/pg-chat/components/ChatSender.tsx
+++
@@ -1,9 +0,0 @@
-function ChatSender({ message }: { message: string }) {
-	return (
-		<div className="flex justify-end">
-			<div className="max-w-[70%] bg-[#f4f4f4] rounded-sm p-4">{message}</div>
-		</div>
-	);
-}
-
-export default ChatSender;
